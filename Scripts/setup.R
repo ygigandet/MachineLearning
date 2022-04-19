c555f6ed9a8ccfe64a7bbb8c023105d273f8db11
ygigandet <yooby.gigandet@gmail.com>
2022-04-19 18:50:34 +0200

    setup.R and eda.Rmd updated
    setup.R - allow the knitr options
    eda.Rmd - Start with the team_stats

--- Scripts/setup.R
+++ Scripts/setup.R
@@ -7,7 +7,7 @@ packages <- c(
   "here", # for the project's organization
   "tidyverse", "kableExtra",
   "usethis", # git
-  "plot_ly" #for plotting
+  "plotly" #for plotting
 )
 
 purrr::walk(packages, library, character.only = TRUE)
@@ -29,18 +29,18 @@ options(
 theme_set(theme_light())
 
 # knitr options
-# opts_chunk$set(
-#  comment = "#>",
-#  collapse = TRUE,
-  # cache = TRUE,
-#  fig.retina = 0.8, # figures are either vectors or 300 dpi diagrams
-#  dpi = 300,
-# out.width = "70%",
-#  fig.align = "center",
-#  fig.width = 6,
-#  fig.asp = 0.618,
-#  fig.show = "hold",
-#  message = FALSE,
-# echo = FALSE
-# )
+opts_chunk$set(
+  comment = "#>",
+  collapse = TRUE,
+  cache = TRUE,
+  fig.retina = 0.8, # figures are either vectors or 300 dpi diagrams
+  dpi = 300,
+  out.width = "70%",
+  fig.align = "center",
+  fig.width = 6,
+  fig.asp = 0.618,
+  fig.show = "hold",
+  message = FALSE,
+ echo = FALSE
+ )
 
